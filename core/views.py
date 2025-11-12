536a365466c3219e3f852b3324d0c6ee4cb77a3e
Ajay Krishna Devadasan <54180151+Ajay998@users.noreply.github.com>
2025-11-12 20:58:16 +0530

    select_related: Query Optimzation

--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .forms import RatingForm, RestaurantForm
-from .models import Restaurant
+from .models import Restaurant, Rating
 # Create your views here.
 
 def submit_rating(request):
@@ -29,10 +29,22 @@ def create_restaurant(request):
     return render(request, 'core/restaurant_created.html', {'form': form})
 
 def index(request):
-    restaurants = Restaurant.objects.prefetch_related('ratings') # Fetch all restaurants with their ratings
+    # restaurants = Restaurant.objects.prefetch_related('ratings', 'sales') # Fetch all restaurants with their ratings and sales in optimized queries
     # SELECT * FROM core_restaurant;
     # SELECT * FROM core_rating WHERE restaurant_id IN (1, 2, 3, ...);
+    # SELECT * FROM core_sale WHERE restaurant_id IN (1, 2, 3, ...);
+
+    restaurants = Restaurant.objects.filter(name__startswith='C').prefetch_related('ratings', 'sales')
+    # SELECT * FROM core_restaurant WHERE name LIKE 'C%';
+    # SELECT * FROM core_rating WHERE restaurant_id IN (1, 2, 3, ...);
+    # SELECT * FROM core_sale WHERE restaurant_id IN (1, 2, 3, ...);
+
     context = {
         'restaurants': restaurants,
     }
-    return render(request, 'core/index.html', context)
+    return render(request, 'core/restaurant_index.html', context)
+
+def rating_index(request):
+    ratings = Rating.objects.select_related('restaurant') # Fetch all ratings with their associated restaurant in optimized query (One to Many relationship)
+    # SELECT * FROM core_rating INNER JOIN core_restaurant ON core_rating.restaurant_id = core_restaurant.id;
+    return render(request, 'core/rating_index.html', {'ratings': ratings})
